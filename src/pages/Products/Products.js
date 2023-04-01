45ecc84d45c143d02da49781162b4309cb48bcf7
Umesh Namdev <67787316+umeshn95@users.noreply.github.com>
2023-04-01 20:07:45 +0530

    Update Products.js

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Ourteam } from '../../components';
 // import { InfoSection } from '../../components';
 import ScrollToTop from '../../components/ScrollToTop';
+import Home from '../HomePage/Home';
 // import { homeObjOne, homeObjTwo } from './Data';
 
 function Products() {
   return (
     <>
-    <Ourteam></Ourteam>
+    <Home></Home>
       
       <ScrollToTop></ScrollToTop>
     </>
